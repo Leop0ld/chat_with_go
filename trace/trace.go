6fc37be49ba96bc27914f78d463780b325d40c8b
Myungseo Kang <l3opold7@gmail.com>
2018-07-12 14:00:04 +0900

    Test Passed :white_check_mark:

--- trace/trace.go
+++ trace/trace.go
@@ -1,7 +1,25 @@
 package trace
 
-// Tracer 는 코드 전체에서 이벤트를 추적할 수 있는 객체를 설명하는 인터페이스
+import (
+	"fmt"
+	"io"
+)
 
+// Tracer 는 코드 전체에서 이벤트를 추적할 수 있는 객체를 설명하는 인터페이스
 type Tracer interface {
 	Trace(...interface{})
+}
+
+// New 함수는 새로운 Tracer 생성함
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
 }
